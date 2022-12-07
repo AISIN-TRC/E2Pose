30f2653d5c34c4875deee2ee18d8113f460d5f44
Masakazu Tobeta <tobeta2012@gmail.com>
2022-12-07 16:40:01 +0900

    Remove Qt

--- live/style.py
+++
@@ -1,8 +0,0 @@
-## coding: UTF-8
-
-# --- Style define ---
-WINDOW_STYLE = "background-color: rgba(0,0,0,255);"
-DOCK_STYLE   = "background: rgba(191,198,220,255);"
-BUTTON_STYLE = "background: white; selection-background-color: gray;"
-COMBO_STYLE  = BUTTON_STYLE
-SLIDER_STYLE = DOCK_STYLE
