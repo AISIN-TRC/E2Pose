6f7b60c289c473dee4d16997d0655df6766902d4
Masakazu Tobeta <tobeta2012@gmail.com>
2022-12-07 15:39:01 +0900

    Add trained models

--- pretrains/download.sh
+++ pretrains/download.sh
@@ -1,1 +1,10 @@
 wget -v -O $(dirname $0)/COCO/ResNet101/512x512/frozen_model.pb -L https://www.dropbox.com/s/ewvbnr85riym2l7/e2pose_coco_resnet101_512.pb?dl=1
+wget -v -O $(dirname $0)/COCO/ResNet101/480x480/frozen_model.pb -L https://www.dropbox.com/s/ps0s34g053ceedx/e2pose_coco_resnet101_480.pb?dl=1
+wget -v -O $(dirname $0)/COCO/ResNet101/448x448/frozen_model.pb -L https://www.dropbox.com/s/aq92bwmmpxx73ej/e2pose_coco_resnet101_448.pb?dl=1
+wget -v -O $(dirname $0)/COCO/ResNet50/320x320/frozen_model.pb -L https://www.dropbox.com/s/ft4w5nd4kpr6yxx/e2pose_coco_resnet50_320.pb?dl=1
+wget -v -O $(dirname $0)/COCO/ResNet50/384x448/frozen_model.pb -L https://www.dropbox.com/s/o6zpqev1u2uzjde/e2pose_coco_resnet50_384x448.pb?dl=1
+wget -v -O $(dirname $0)/COCO/ResNet50/512x512/frozen_model.pb -L https://www.dropbox.com/s/hglzlcnyupkof16/e2pose_coco_resnet50_512.pb?dl=1
+wget -v -O $(dirname $0)/COCO/ResNet152/384x448/frozen_model.pb -L https://www.dropbox.com/s/jtn5sfmdv1a3od7/e2pose_coco_resnet152_384x448.pb?dl=1
+wget -v -O $(dirname $0)/COCO/ResNet152/448x448/frozen_model.pb -L https://www.dropbox.com/s/p69dn0yvltjpqs9/e2pose_coco_resnet152_448.pb?dl=1
+wget -v -O $(dirname $0)/COCO/MobileNetV2/320x320/frozen_model.pb -L https://www.dropbox.com/s/wejat9x18h7rny0/e2pose_coco_mobilenet2_320.pb?dl=1
+wget -v -O $(dirname $0)/COCO/MobileNetV2/448x512/frozen_model.pb -L https://www.dropbox.com/s/089nsj1s953yka3/e2pose_coco_mobilenet2_448x512.pb?dl=1
